824fbb079fceb53bd76bbeebd054a690760b4041
Seokju Hong <iamseokjuhong@gmail.com>
2022-08-23 14:37:26 +0900

    project init

---
+++ src/main/java/com/example/exercisenotification/ExerciseNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercisenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exercisenotification/ExerciseNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercisenotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
